9c89a537c781dca5af6af6947ce018bb34009ab6
Nada Abdelnasser <102680067+NadaAbdelnasser@users.noreply.github.com>
2023-07-23 23:16:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// let navbar = document.querySelector('.navbar');
+// let icon = document.querySelector('#toggler');
+
+// icon.addEventListener('click', function(){
+//     navbar.classList.toggle('active');
+// })
